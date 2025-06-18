9cd0885fe37073715a2d71c15e4f2358347a9827
thesongmartins <martinsjehoshaphat@gmail.com>
2025-06-18 02:12:29 +0100

    Half way through cards components

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,11 +1,21 @@
-// import Button from "./Button";
+import Button from "./Button";
 
 const Card = () => {
   return (
-    <div className="h-64 w-56 grid grid-rows-2 row-span-7 bg-white rounded-2xl shadow-lg">
+    <div className="h-64 w-56 grid grid-rows-2 bg-white rounded-2xl shadow-lg">
       <img src="/assets/Coffee.jpg" alt="" />
-      <div className="w-full grid grid-cols-2 "></div>
-      {/* <Button text="Tags" /> */}
+      <div className="w-full grid grid-cols-2 p-2 ">
+        <div>
+          <h2 className="">Coffee</h2>
+          <div>
+            <img src="/resources/Star.svg" alt="" />
+            <span>4.7(65 votes)</span>
+          </div>
+        </div>
+        <div>
+          <Button text="Tags" />
+        </div>
+      </div>
     </div>
   );
 };
